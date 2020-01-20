d5b89d32f8107b85e202728b972834c2bfe4e096
richardssinclair <richardssinclair@gmail.com>
2020-01-20 15:35:27 +0000

    Automated commit at 2020-01-20 15:35:27 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,12 @@
-// This is where it all goes :)
+$(document).ready(function(){
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // fade: true,
+        // centerMode: true,
+        // variableWidth: true,
+        // infinite: true,
+        // speed: 300,
+        arrows: true
+      });
+  });
